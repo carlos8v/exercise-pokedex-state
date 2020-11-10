b5c5bb7141047f394927cd0f40c95947750ed743
Carlos Souza <carlos.pessoal@hotmail.com>
2020-11-10 18:10:13 -0300

    Primeiro requisito do exercício

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -2,10 +2,27 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 class Pokedex extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pokedex: this.props.pokemons,
+            currentPokemon: 0,
+        }
+    }
+
+    handleNextPokemon = () => {
+        this.setState(({ currentPokemon, pokedex }) => ({
+            currentPokemon: currentPokemon >= pokedex.length - 1 ? 0: currentPokemon + 1,
+        }));
+    }
+
     render() {
         return (
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+                <Pokemon pokemon={this.state.pokedex[this.state.currentPokemon]} />
+                <div>
+                    <button onClick={this.handleNextPokemon}>Próximo</button>
+                </div>
             </div>
         );
     }
