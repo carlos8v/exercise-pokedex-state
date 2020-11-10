3f44bf4f919b361ae5a53ed663ae241ca5678317
Carlos Souza <carlos.pessoal@hotmail.com>
2020-11-10 20:26:50 -0300

    Requisitos bônus

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import pokemons from './data';
-import Pokedex from './Pokedex';
+import Pokedex from './components/Pokedex';
 
 function App() {
   return (
     <div className="App">
-      <h1> Pokedex </h1>
+      <h1 className="pokedex__header"> Pokedex </h1>
       <Pokedex pokemons={pokemons} />
     </div>
   );
